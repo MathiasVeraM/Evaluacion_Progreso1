0e801e6b47970f2cc0878714dff7ee30818c45a0
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-03 14:53:04 -0500

    Indicaciones del programa a desarrollar

---
+++ Evaluacion_Progreso1.c
@@ -0,0 +1,4 @@
+/*Realice un programa en el que dado n números naturales, determine y escriba qué porcentaje son pares y que porcentaje son primos.
+Por ejemplo:
+Entrada: 4 5 6 2 1 -5 9 8
+Salida: Pares: 50 %, Primos: 25%*/
