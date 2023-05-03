d6dd3f15f9de6aa6bb4d57e3fc8cabaa068137ca
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-03 15:28:27 -0500

    Cambio de planes, le hice en el mismo ciclo la evaluacion de par o primo

--- Evaluacion_Progreso1.c
+++ Evaluacion_Progreso1.c
@@ -4,19 +4,27 @@ Entrada: 4 5 6 2 1 -5 9 8
 Salida: Pares: 50 %, Primos: 25%*/
 //Incluir la biblioteca stdio.h
 #include <stdio.h>
+
 int main(){
-    int n = 0, pares=0, primos=0, i;
+    int n = 0, pares=0, primos=0, i, total=0, numerospares=0, numerosprimos=0;
+    int par=0, primo=0;
     do{
-        printf("Por favor ingrese un numero: ");
-        scanf("%d",&n);
-        printf("\ndesea continuar?, pulse 1 para seguir y 0 para parar: ");
-        scanf("%d",&i);
+       //Pedir al usuario los valores, y comprobar que quiera seguir con el programa
+       printf("Por favor ingrese un numero: ");
+       scanf("%d",&n);
+       total++;
+        if(n%2==0){
+            par++;
+        }
+        for(int c = 1;c <= n;c++){
+            if(n%c==0){
+                primo++;;
+            }
+        }
+       printf("\ndesea continuar?, pulse 1 para seguir y 0 para parar: ");
+       scanf("%d",&i);
     }while(i!=0);
-    
-
-
-
-
-
+    printf("Fueron %d numeros pares ingresados", par);
+    printf("\nFueron %d numeros primos ingresados", primo);
     return 0;
 }
